9586c9580e8e714279d0a53f71a5ca9e36c686ca
sibela46 <sibelachinareva@gmail.com>
2017-11-12 10:58:04 +0000

    sheet 6 finished

--- Sheet6/Parser.hs
+++ Sheet6/Parser.hs
@@ -1,5 +1,6 @@
 import Yoda
 import Control.Applicative
+import Prelude hiding ((++))
 
 data Maybe' a = Nothing'
               | Just' a
@@ -9,7 +10,10 @@ instance Functor Maybe' where
   fmap f (Just' x) = Just' (f x)
 
 instance Applicative Maybe' where
-  pure x = Nothing'
+  pure x = Just' x
+  Nothing' <*> Nothing' = Nothing'
+  Nothing' <*> Just' x = Nothing'
+  Just' f <*> Nothing' = Nothing'
   Just' f <*> Just' x = Just' (f x)
 
 data List a = Empty
@@ -23,9 +27,14 @@ instance Functor List where
 x <$ Empty = Cons x Empty
 x <$ Cons y ys = Cons x Empty
 
+(++) :: List a -> List a -> List a
+Empty ++ x = x
+Cons x xs ++ ys = Cons x (xs ++ ys)
+
 instance Applicative List where
-  pure x = Empty
-  Cons f xs <*> Cons y ys = Cons (f y) (Cons f xs <*> ys)
+  pure x = Cons x Empty
+  Empty <*> x = Empty
+  Cons f fs <*> xs = (fmap f xs) ++ (fs<*>xs)
 
 (<*) :: List a -> List b -> List a
 Empty <* Cons x xs = Empty
@@ -33,15 +42,15 @@ Cons x xs <* Empty = Cons x xs
 Cons x xs <* Cons y ys = Cons x xs
 
 (*>) :: List a -> List b -> List b
-Empty *> Cons x xs = Cons x xs
 Cons x xs *> Empty = Empty
+Empty *> Cons x xs = Cons x xs
 Cons x xs *> Cons y ys = Cons y ys
 
 instance Alternative List where
   empty = Empty
   Empty <|> x = x
   x <|> Empty = x
-  (Cons x xs) <|> (Cons y ys) = (Cons x xs) ++ (Cons y ys) where
-    (++) :: List a -> List a -> List a
-    Empty ++ xs = xs
-    Cons y ys ++ xs = Cons y (ys ++ xs)
+  (Cons x xs) <|> (Cons y ys) = (Cons x xs) ++ (Cons y ys)
+
+liftATwo :: Applicative f => (a->b->c) -> f a -> f b -> f c
+liftATwo f x y = f <$> x<*>y
