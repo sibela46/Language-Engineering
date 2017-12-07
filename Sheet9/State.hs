275e80ac5c55ea4e8d620bf85aa9ea8a6186648a
Sibela Chinareva <sc16913@it025965.users.bris.ac.uk>
2017-12-07 18:06:35 +0000

    lab

--- Sheet9/State.hs
+++ Sheet9/State.hs
@@ -1,4 +1,5 @@
 import Data.Monoid
+import Control.Applicative
 
 data State s a = State (s -> (s, a))
 
@@ -29,4 +30,42 @@ put :: s -> State s ()
 put x = State (\_ -> (x, ()))
 
 add :: Int -> Int -> State Int Int
-add x y = State (\s -> (s, x+y))
+add x y = State (\s -> (s+x, x+y))
+
+operationMlt :: Int -> Int -> State Int Int
+operationMlt x y = State (\s -> (s+x, x*y))
+
+runOp :: Int -> Int -> (Int, Int)
+runOp x y = runState (add x y) 0
+
+type MiniImp = State (Int, Int)
+
+getX :: MiniImp Int
+getX = fst <$> get
+
+getY :: MiniImp Int
+getY = snd <$> get
+
+setX :: Int -> MiniImp ()
+setX x = do (_,y) <- get
+            put(x,y)
+
+setY :: Int -> MiniImp ()
+setY y = do (x,_) <- get
+            put(x,y)
+
+while :: ((Int, Int) -> Bool) -> ((Int,Int) -> (Int,Int)) -> MiniImp ()
+while p f =
+  do
+    xy <- get
+    if p xy
+      then do
+        put (f xy)
+        while p f
+    else return ()
+
+fact :: Int -> Int
+fact x =  fst $ execState (while ((>0).snd) (\(x, y)-> (x*y, y-1))) (1, x)
+
+liftM2 :: Monad m => (a->b->c) -> m a -> m b -> m c
+liftM2 f mx my = (mx >>= f)
